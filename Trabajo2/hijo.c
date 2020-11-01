d58741811e58e3f0814922febd8b6c48b27c2fd7
Israel Palma Sebastià <38512074+israelps95@users.noreply.github.com>
2020-11-01 19:23:46 +0100

    Tot funcionant
    Falta revisar alguns mètodes i comprobar que tot es mostra ok!

--- Trabajo2/hijo.c
+++ Trabajo2/hijo.c
@@ -1,175 +1,198 @@
 //este archivo es el fichero fuente que al compilarse produce el ejecutable HIJO
-#include <errno.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+#include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/msg.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <string.h>
 
-//VARIABLES GLOBALES
+//Varible estado
 char estado[4] = "";
+
+int add_pid_lista(int *array_lista, int n_procesos)
+{
+  for(int i = 0; i < n_procesos; i++)
+    if(array_lista[i] == 0) 
+      { array_lista[i] = getpid(); return 0;}
+  return 1;
+}
+
+int pid_aleatorio(int *array_lista, int n_procesos)
+{
+  int n_pid = 1 + rand()%n_procesos;	// Numero al azar entre 1 y el numero de pids.
+  int count = 0;
+  while(1)
+  {
+    for(int i = 0; i < n_procesos; i++)
+    {
+      if(array_lista[i] > 0 && array_lista[i] != getpid()) count++;	// Comprobamos si el pid esta vivo y no es el mismo pid.
+      if(count == n_pid) return array_lista[i];				// Devolvemos el pid elegido al azar.
+    }
+  }
+}
 
 //Función defensa
 void defensa() {
-    printf("El hijo %d ha repelido un ataque.\n", getpid());
+    printf("El hijo %d ha repelido un ataque \n");
     fflush(stdout);
     strcpy(estado, "OK");
 }
 
 //Función indefenso
 void indefenso() {
-    printf("El hijo %d ha sido emboscado mientras realizaba un ataque.\n", getpid());
+    printf("El hijo %d ha sido emboscado mientras realizaba un ataque \n");
     fflush(stdout);
     strcpy(estado, "KO");
 }
 
-//Función para elegir el pid del hijo atacado
-int hijoAtacado(int *arrayLista, int numProcesos) {
-    int res = 1 + (rand()%numProcesos);
-    for(int i = 0; i < numProcesos; i++) {
-        if(arrayLista[i] != getpid() && arrayLista[i] == res) {
-            return res;
-        }
+//MÉTODOS AUXILIARES PARA EL SEMÁFORO
+//Inicializar el semáforo    
+int init_sem(int semid, short valor) {
+    short sem_array[1];
+    sem_array[0] = valor;
+    if(semctl(semid, 0, SETALL, sem_array) == -1) {
+        perror("Error semctl");
+        return -1;
     }
+    return 0;
+} 
+
+//Semáforo cerrado
+int wait_sem(int semid) {
+    struct sembuf op[1];
+    op[0].sem_num = 0;
+    op[0].sem_op = -1;
+    op[0].sem_flg = 0;
+    if(semop(semid, op, 1) == -1) {
+        perror("Error semop:");
+        return -1;
+    }
+    return 0;
 }
 
-//FUNCIONES PARA EL SEMÁFORO
-//Iniciar semáforo
-void iniciarSem(int idSem, int valor) {
-    if(semctl(idSem, 0, SETVAL, valor) == -1) {
-        perror("Error al iniciar el semáforo");
-        exit(1);
+//Semáforo abierto
+int signal_sem(int semid) {
+    struct sembuf op[1];
+    op[0].sem_num = 0;
+    op[0].sem_op = 1;
+    op[0].sem_flg = 0;
+
+    if(semop(semid, op, 1) == -1) {
+        perror("Error semop:");
+        return -1;
     }
-}
-
-//Abrir semáforo
-void abrirSem(int idSem) {
-    struct sembuf op;
-    op.sem_num = 0;
-    op.sem_op = 1;
-    op.sem_flg = 0;
-
-    if(semop(idSem, &op, 1) == -1) {
-        perror("Error al abrir el semáforo");
-        exit(1);
-    }
-}
-
-//Cerrar semáforo
-void cerrarSem(int idSem) {
-    struct sembuf op;
-    op.sem_num = 0;
-    op.sem_op = -1;
-    op.sem_flg = 0;
-
-    if(semop(idSem, &op, 1) == -1) {
-        perror("Error al cerrar el semáforo");
-        exit(1);
-    }
-}
+    return 0;
+} 
 
 //MÉTODO MAIN
 int main(int argc, char *argv[]) {
-    //VARIABLES
-    key_t clave;
+    srand(getpid());
+
+    //VARIABLES GLOBALES
+    key_t llave;
     int mensajes;
     int lista;
-    int numProcesos = atoi(argv[1]);
+    int nPIDs = atoi(argv[1]);
     int sem;
     int barrera[2];
 
-    srand(getpid());
-
-    //Crear clave asociada al ejecutable
-    clave = ftok(argv[0], 'X');
-    if(clave == -1) {
-        perror("Error al crear la clave");
-        exit(1);
-    }
-
-    //Crear cola de mensajes
-    mensajes = msgget(clave, 0600 | IPC_CREAT);
-    if (mensajes == -1) {
-        perror("Error al crear la cola de mensajes");
-        exit(1);
-    }
-
-    //Crear la estructura de mensaje que será enviado
     struct {
+        long tipo;
         int pid;
-        char estado[2];
+        char estado[4];
     } mensaje;
 
-    //Crear región de memoria compartida
-    lista = shmget(clave, numProcesos*sizeof(int), IPC_CREAT | 0600);
-    if (lista == -1) {
-        perror("Error al crear la región de memoria compartida");
+    //Creamos una clave asociada al fichero ejecutable
+    if((llave = ftok(argv[0], 'X')) == -1) {
+        perror("ftok");
         exit(1);
     }
 
-    //Crear array para la memoria compartida
-    int *arrayLista = (int *)shmat (lista, (char *)0, 0);
+    printf("ftok: %d \n", llave);
+    fflush(stdout);
 
-    //Crear semáforo
-    sem = semget(clave, 1, IPC_CREAT | 0600);
+    //Creamos una cola de mensajes
+    mensajes = msgget(llave, IPC_CREAT | 0600);
+    if (mensajes == -1) {
+        perror("semid");
+        exit(2);
+    }
+    int longitud = sizeof(mensaje) - sizeof(mensaje.tipo);
+
+    //Creamos la región de memoria compartida
+    lista = shmget(llave, nPIDs*sizeof(int), IPC_CREAT | 0600);
+    if(lista == -1) {
+        perror("shmget");
+        exit(2);
+    }
+
+    //Asignamos al array el puntero a la memoria compartida
+    int *array = shmat(lista, 0, 0);
+
+    //Creamos el semáforo
+    sem = semget(llave, 1, IPC_CREAT | 0600);
     if(sem == -1) {
-        perror("Error al crear el semáforo");
+        perror("semget");
         exit(1);
     }
+    
+    printf("array: %d\n  sem: %d \n", array, sem);
+    fflush(stdout);
 
-    //Iniciar semáforo
-    iniciarSem(sem, 1);
-
-    //Iniciar barrera
+    //Recibimos los valores de la tubería
     barrera[0] = atoi(argv[2]);
     barrera[1] = atoi(argv[3]);
 
-    //Añadimos el pid del hijo en la memoria compartida usando el semáforo
-    cerrarSem(sem);
-    for(int i = 0; i < numProcesos; i++){
-        if(arrayLista[i] == 0) {
-            arrayLista[i] = getpid();
-        }
-    }
-    abrirSem(sem);
+    //Añadimos el PID al array
+    wait_sem(sem);
+    printf("%d - %d \n", nPIDs, getpid());
+    fflush(stdout);
+    add_pid_lista(array, nPIDs);
+    signal_sem(sem);
 
     char K;
     while(1) {
+
         read(barrera[0], &K, sizeof(K));
-        
-        int decision = (rand()%2);
+        int decision = rand() % 2;
+        printf("dec %d \n", decision);
+        fflush(stdout);
+        //Ataca
         if(decision == 0) {
             if(signal(SIGUSR1, indefenso) == SIG_ERR) {
-                perror("Error al asignar indefenso");
-                exit(1);
+                perror("error al atacar");
+                exit(2);
             }
             usleep(100000);
-            int pidAtacado = hijoAtacado(arrayLista, numProcesos);
-            printf("El proceso %d, atacando al proceso %d\n", getpid(), pidAtacado);
+            //Elegimos al hijo al que atacaremos
+            int pidAtacado = pid_aleatorio(array, nPIDs);
+            printf("Atacando al proceso %d", pidAtacado);
             fflush(stdout);
             kill(pidAtacado, SIGUSR1);
             usleep(100000);
         } else if(decision == 1) {
             if(signal(SIGUSR1, defensa) == SIG_ERR) {
-                perror("Error al asignar defensa");
-                exit(1);
+                perror("error al defender");
+                exit(2);
             }
             usleep(200000);
         } else {
-            perror("Error asignando ataque o defensa en un hijo");
+            perror("error eligiendo ataque o defensa");
+            exit(2);
         }
+
+        mensaje.tipo = 1;
         mensaje.pid = getpid();
         strcpy(mensaje.estado, estado);
-        if(msgsnd(mensajes, &mensaje, sizeof(mensaje), 0) == -1) {
-            perror("Error en la comunicación con el padre");
-            exit(1);
+        if(msgsnd(mensajes, &mensaje, longitud, 0) == -1) {
+            perror("error en msgsnd");
+            exit(2);
         }
     }
-    return 0;
 }
--- Trabajo2/padre.c
+++ Trabajo2/padre.c
@@ -1,200 +1,240 @@
 //este archivo es el fichero fuente que al compilarse produce el ejecutable PADRE
-#include <errno.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+#include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/msg.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <sys/msg.h>
+#include <stdio.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <string.h>
 
-//FUNCIONES PARA EL SEMÁFORO
-//Iniciar semáforo
-void iniciarSem(int idSem, int valor) {
-    if(semctl(idSem, 0, SETVAL, valor) == -1) {
-        perror("Error al iniciar el semáforo");
-        exit(1);
+///MÉTODOS AUXILIARES PARA EL SEMÁFORO
+//Inicializar el semáforo    
+int init_sem(int semid, short valor) {
+    short sem_array[1];
+    sem_array[0] = valor;
+    if(semctl(semid, 0, SETALL, sem_array) == -1) {
+        perror("Error semctl");
+        return -1;
     }
+    return 0;
 }
 
-//Abrir semáforo
-void abrirSem(int idSem) {
-    struct sembuf op;
-    op.sem_num = 0;
-    op.sem_op = 1;
-    op.sem_flg = 0;
-
-    if(semop(idSem, &op, 1) == -1) {
-        perror("Error al abrir el semáforo");
-        exit(1);
+//Semáforo cerrado
+int wait_sem(int semid) {
+    struct sembuf op[1];
+    op[0].sem_num = 0;
+    op[0].sem_op = -1;
+    op[0].sem_flg = 0;
+    if(semop(semid, op, 1) == -1) {
+        perror("Error semop:");
+        return -1;
     }
+    return 0;
 }
 
-//Cerrar semáforo
-void cerrarSem(int idSem) {
-    struct sembuf op;
-    op.sem_num = 0;
-    op.sem_op = -1;
-    op.sem_flg = 0;
+//Semáforo abierto
+int signal_sem(int semid) {
+    struct sembuf op[1];
+    op[0].sem_num = 0;
+    op[0].sem_op = 1;
+    op[0].sem_flg = 0;
 
-    if(semop(idSem, &op, 1) == -1) {
-        perror("Error al cerrar el semáforo");
-        exit(1);
+    if(semop(semid, op, 1) == -1) {
+        perror("Error semop:");
+        return -1;
     }
-}
+    return 0;
+} 
 
 //MÉTODO MAIN
 int main(int argc, char *argv[]) {
 
-    //VARIABLES
-    key_t clave;
+    //VARIABLES GLOBALES
+    key_t llave;
     int mensajes;
     int lista;
-    int numProcesos = strtol(argv[1], NULL, 10);
+    int nPIDs = strtol(argv[1], NULL, 10);
     int sem;
     int barrera[2];
 
-    //Crear clave asociada al ejecutable
-    clave = ftok(argv[0], 'X');
-    if(clave == -1) {
-        perror("Error al crear la clave");
-        exit(1);
-    }
 
-    //Crear cola de mensajes
-    mensajes = msgget(clave, 0600 | IPC_CREAT);
-    if (mensajes == -1) {
-        perror("Error al crear la cola de mensajes");
-        exit(1);
-    }
-
-    //Crear la estructura de mensaje que será enviado
     struct {
+        long tipo;
         int pid;
         char estado[4];
     } mensaje;
     
-    //Crear región de memoria compartida
-    lista = shmget(clave, numProcesos*sizeof(int), IPC_CREAT | 0600);
-    if (lista == -1) {
-        perror("Error al crear la región de memoria compartida");
+
+    //Creamos una clave asociada al fichero ejecutable
+    if((llave = ftok(argv[0], 'X')) == -1) {
+        perror("ftok");
         exit(1);
     }
 
-    //Crear array para la memoria compartida
-    int *arrayLista = (int *)shmat (lista, (char *)0, 0);
+    printf("ftok: %d \n", llave);
+    fflush(stdout);
 
-    //Crear semáforo
-    sem = semget(clave, 1, IPC_CREAT | 0600);
+    //Creamos una cola de mensajes
+    mensajes = msgget(llave, IPC_CREAT | 0600);
+    if (mensajes == -1) {
+        perror("semid");
+        exit(2);
+    }
+    int longitud = sizeof(mensaje) - sizeof(mensaje.tipo);
+
+    //Creamos la región de memoria compartida
+    lista = shmget(llave, nPIDs*sizeof(int), IPC_CREAT | 0600);
+    if(lista == -1) {
+        perror("shmget");
+        exit(2);
+    }
+
+    //Asignamos al array el puntero a la memoria compartida
+    int *array = shmat(lista, 0, 0);
+
+    //Creamos el semáforo
+    sem = semget(llave, 1, IPC_CREAT | 0600);
     if(sem == -1) {
-        perror("Error al crear el semáforo");
+        perror("semget");
         exit(1);
     }
 
-    //Iniciar semáforo
-    iniciarSem(sem, 1);
+    //Inicializar el semáforo
+    init_sem(sem, 1);
 
-    //Crear tubería
+    //Creamos la tubería
     if(pipe(barrera) == -1) {
-        perror("Error al crear la tubería");
-        exit(1);
+        perror("pipe");
+        exit(-1);
     }
 
-    //Convertir la tubería a char para que se pueda pasar a los hijos
-    char barrera0[10], barrera1[10];
+    printf("array: %d\n  sem: %d \n", array, sem);
+    fflush(stdout);
+
+    //Convertimos la tubería para que se le pueda pasar como parametro a los hijos
+    char barrera0[10];
+    char barrera1[10];
     sprintf(barrera0, "%d", barrera[0]);
     sprintf(barrera1, "%d", barrera[1]);
 
-    //Crear N hijos
-    for(int i = 0; i < numProcesos; i++) {
-        int hijo = fork();
-        if(hijo == -1) {
-            perror("Error al crear un hijo");
-            exit(1);
-        } else if(hijo == 0) {
-            char *info[] = {argv[0], argv[1], barrera0, barrera1};
+    //Creación de los hijos
+    for(int i = 0; i < nPIDs; i++) {
+        int pidHijo;
+        if((pidHijo = fork()) == -1) {
+            perror("fork");
+            exit(-1);
+        } else if(pidHijo == 0) {
+            char *info[] = {argv[0], argv[1], barrera0, barrera1, NULL};
             execvp("./Trabajo2/HIJO", info);
             exit(0);
         }
     }
 
-    //Iniciar rondas de ataque
-    int hijosVivos = numProcesos;
-    char K = 'k';
-    while(hijosVivos > 1) {
-        printf("Iniciando la ronda de ataques\n");
-        fflush(stdout);
-        //Avisar a los contendientes para que se preparen
-        for(int i = 0; i < hijosVivos; i++) {
-            write(barrera[1], &K, sizeof(K));
-        }
-        for(int i = 0; i < hijosVivos; i++) {
-            
-            //Recibir mensaje de los hijos vivos
-            if(msgrcv(mensajes, &mensaje, sizeof(mensaje), 1, 0) == -1) {
-                perror("Error en la comunicación con un hijo");
-            }
-            
-            //Matar si el proceso a muerto
-            if(strcmp(mensaje.estado, "KO") == 0) {
-                waitpid(mensaje.pid, NULL, 0);
-                printf("Proceso %d FINALIZADO\n", mensaje.pid);
-                fflush(stdout);
-
-                //Sacamos el hijo muerto de la memoria compartida
-                cerrarSem(sem);
-                for(int i = 0; i < numProcesos; i++) {
-                    if(mensaje.pid == arrayLista[i]) {
-                        arrayLista[i] = 0;
-                    }
-                }
-                abrirSem(sem);
-            }
-        }
-
-        //Comprobamos cuantos hijos quedan vivos
-        int vivosActuales = 0;
-        for(int i = 0;  i < numProcesos; i++) {
-            if(arrayLista[i] > 0) {
-                vivosActuales++;
-            }
-        }
-        hijosVivos = vivosActuales;
-        printf("Finalizando la rondad de ataques\n");
-        fflush(stdout);
+    int fin = 1;
+    while(fin == 1)
+    {
+        fin = 0;
+        for(int i = 0; i < nPIDs; i++)
+            if(array[i] == 0) fin = 1;
+                usleep(100000);
     }
     
-    //Accedemos al fichero FIFO resultado
-    FILE *resultadoFile;
-    resultadoFile = fopen("resultado", "a");
+    // Mostramos por pantalla los PIDs creados inicialmente.
+    printf("*************************\n");
+    printf("*     PIDs INICIALES    *\n");
+    for(int i = 0; i < nPIDs; i++) printf("* pid[%d] = %d\t\t*\n", i, array[i]);
+    printf("*************************\n\n");
+    fflush(stdout);
 
-    if(hijosVivos == 1) {
-        for(int i = 0; i < numProcesos; i++) {
-            kill(arrayLista[i], SIGTERM);
-            waitpid(arrayLista[i], NULL, 0);
-            fprintf(resultadoFile, "El hijo %d ha ganao", arrayLista[i]);
-            fflush(stdout);
-        }
-    } else {
-        fprintf(resultadoFile, "Empate");
+    //Empezamos con los combates
+    int pidsActivos = nPIDs;
+    char K = 'a';
+    while(pidsActivos > 1) {
+        printf("Iniciando ronda de ataques \n");
         fflush(stdout);
+        for(int i = 0; i < pidsActivos; i++) {
+            //Enviamos K bytes por la barrera
+            write(barrera[1], &K, sizeof(K));
+        }
+
+        for(int i = 0; i < pidsActivos; i++) {
+            //Recibimos el mensaje de los hijos
+            if(msgrcv(mensajes, &mensaje, longitud, 1, 0) == -1) {
+                perror("msgrcv");
+                exit(3);
+            }
+
+            //Comprobamos si el hijo a muerto
+            if(strcmp(mensaje.estado, "KO") == 0) {
+                //Matamos el PID del hijo muerto
+                kill(mensaje.pid, SIGTERM);
+                //Esperamos a que el hijo muera
+                waitpid(mensaje.pid, NULL, 0);
+                //Sacamos al hijo muerto del array
+                wait_sem(sem);
+                for(int i = 0; i < nPIDs; i ++) {
+                    if(mensaje.pid == array[i]) {
+                        array[i] = 0;
+                    }
+                }
+                signal_sem(sem);
+            }
+        }
+        //Actualizamos el número de pids activos
+        int res = 0;
+        for(int i = 0; i < nPIDs; i++) {
+            if(array[i] > 0) {
+                res++;
+            }
+        }
+        pidsActivos = res;
+        printf("Finalizando la ronda de ataques \n");
+        fflush(stdout);
+    } 
+
+    //Abrimos el fichero FIFO resultado
+    FILE *ficheroResulado;
+    ficheroResulado = fopen("resultado", "a");
+
+    //Comprobamos si tenemos un ganador o ha sido empate
+    if(pidsActivos == 1) {
+        for(int i = 0; i < nPIDs; i++) {
+            if(array[i] > 0) {
+                //Matamos el PID que queda
+                kill(mensaje.pid, SIGTERM);
+                //Esperamos a que el hijo muera
+                waitpid(mensaje.pid, NULL, 0);
+                //Imprimimos en el fichero al ganador
+                fprintf(ficheroResulado, "El hijo %d ha ganadao", array[i]);
+            } else {
+                //Imprimimos en el fichero el empate
+                fprintf(ficheroResulado, "Empate");
+            }
+        }
     }
 
-    //Cerrar el semáforo
-    semctl(sem, IPC_RMID, 0);
-    //Cerrar la cola de mensajes
-    msgctl(mensajes, IPC_RMID,0);
-    shmdt(arrayLista);
-    //Cerrar la memoria compartida
+    //Cerramos la cola de mensajes
+    msgctl(mensajes, IPC_RMID, 0);
+
+    //Cerramos el array y la memoria compartida
+    shmdt(array);
     shmctl(lista, IPC_RMID, 0);
+
+    //Cerramos el semáforo
+    semctl(sem, IPC_RMID, 0);
+
     //Cerramos la tubería
     close(barrera[0]);
     close(barrera[1]);
+
+    //Comprobamos que los recursos hayan sido cerrados
+    system("ipcrm -a");  
+    system("ipcs -qs");
 
     return 0;
 }
